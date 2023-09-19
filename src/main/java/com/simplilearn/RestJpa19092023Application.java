360c04613a7759300b6e1001497bbca107ccb390
Dhruvik <dhruviksparikh@gmail.com>
2023-09-19 11:23:59 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/RestJpa19092023Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJpa19092023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJpa19092023Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/entity/Movie.java
@@ -0,0 +1,45 @@
+package com.simplilearn.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movie_19092023")
+public class Movie {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name = "movie_name")
+	private String name;
+	
+	@Column(name = "movie_genre")
+	private String genre;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+}
